89e4f83d2c7e1cf5c8bd07d5f717c98281ae3c65
Carlos Mastalli <carlos.mastalli@iit.it>
2014-04-24 16:51:45 +0200

    Modified YELLOW color for bold letters

--- include/utils/macros.h
+++ include/utils/macros.h
@@ -2,7 +2,7 @@
 
 #define RED     "\033[1m\033[31m"
 #define GREEN   "\033[32m"
-#define YELLOW  "\x1b[33m"
+#define YELLOW  "\033[1m\033[33m"
 #define BLUE    "\033[1m\033[34m"
 #define MAGENTA "\x1b[35m"
 #define CYAN    "\x1b[36m"
