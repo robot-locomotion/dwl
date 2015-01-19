0ad031856f6c6cb76a61d9f7bc8c24db598244a8
Carlos Mastalli <carlos.mastalli@iit.it>
2015-01-19 13:22:26 +0100

    Read parameters for publishing a flat terrain.

--- include/terrain_server/DefaultFlatTerrain.h
+++ include/terrain_server/DefaultFlatTerrain.h
@@ -8,7 +8,7 @@ namespace terrain_server
 class DefaultFlatTerrain
 {
 	public:
-		DefaultFlatTerrain();
+		DefaultFlatTerrain(ros::NodeHandle node);
 		~DefaultFlatTerrain();
 
 		void setFlatTerrain();
@@ -18,11 +18,23 @@ class DefaultFlatTerrain
 		/** @brief ROS node handle */
 		ros::NodeHandle node_;
 
+		/** @brief Private ROS node handle */
+		ros::NodeHandle private_node_;
+
 		/** @brief Flat terrain point cloud publisher */
 		ros::Publisher flat_terrain_pub_;
 
 		/** @brief Default flat terrain service */
 //		ros::ServiceServer flat_terrain_srv_;
+
+		double x_min_;// = -0.50;
+		double y_min_;// = -0.35;
+		double x_max_;// = 0.80;
+		double y_max_;// = 0.35;
+		double resolution_;// = 0.01;
+		double height_;// = -0.54;
+
+		std::string world_frame_;
 
 		typedef pcl::PointCloud<pcl::PointXYZ> PointCloud;
 
--- src/DefaultFlatTerrain.cpp
+++ src/DefaultFlatTerrain.cpp
@@ -1,14 +1,21 @@
 #include <terrain_server/DefaultFlatTerrain.h>
-//#include "pcl_ros/point_cloud.h"
+
+
 
 namespace terrain_server
 {
 
-DefaultFlatTerrain::DefaultFlatTerrain()
+DefaultFlatTerrain::DefaultFlatTerrain(ros::NodeHandle node) : node_(node), x_min_(0.0), x_max_(0.0),
+		y_min_(0.0), y_max_(0.0), resolution_(0.0), height_(0.0), world_frame_("odom")
 {
-
-//	flat_terrain_srv_ = node_.advertiseService("terrain_server/default_flat_terrain", &DefaultFlatTerrain::setFlatTerrain, this);
-	flat_terrain_pub_  = node_.advertise<sensor_msgs::PointCloud2>("head_mount_asus_xtion/depth_registered/points", 1);//TODO read topic
+	flat_terrain_pub_  = private_node_.advertise<sensor_msgs::PointCloud2>("topic_output", 1);
+	node_.param("world_frame", world_frame_, world_frame_);
+	node_.param("x_min", x_min_, x_min_);
+	node_.param("x_max", x_max_, x_max_);
+	node_.param("y_min", y_min_, y_min_);
+	node_.param("y_max", y_max_, y_max_);
+	node_.param("height", height_, height_);
+	node_.param("resolution", resolution_, resolution_);
 }
 
 
@@ -20,30 +27,22 @@ DefaultFlatTerrain::~DefaultFlatTerrain()
 
 void DefaultFlatTerrain::setFlatTerrain()
 {
-	double x_min = -1;
-	double y_min = -1;
-	double x_max = 1;
-	double y_max = 1;
-	double resolution = 0.01;
-	double z = -0.54;
-
-
 	// Computing the boundary properties
-	double center_x = x_min + (x_max - x_min) / 2;
-	double center_y = y_min + (y_max - y_min) / 2;
-	double window_x = (x_max - x_min) / 2;
-	double window_y = (y_max - y_min) / 2;
+	double center_x = x_min_ + (x_max_ - x_min_) / 2;
+	double center_y = y_min_ + (y_max_ - y_min_) / 2;
+	double window_x = (x_max_ - x_min_) / 2;
+	double window_y = (y_max_ - y_min_) / 2;
 
 	PointCloud pcl_msg;
-	pcl_msg.header.frame_id = "odom";
-	for (double xi = 0; xi < window_x; xi += resolution) {
+	pcl_msg.header.frame_id = "world";
+	for (double xi = 0; xi < window_x; xi += resolution_) {
 		for (int sx = -1; sx <= 1; sx += 2) {
-			for (double yi = 0; yi < window_y; yi += resolution) {
+			for (double yi = 0; yi < window_y; yi += resolution_) {
 				for (int sy = -1; sy <= 1; sy += 2) {
 					double x = sx * xi + center_x;
 					double y = sy * yi + center_y;
 //					pcl_msg.height = pcl_msg.width = 1;
-					pcl_msg.push_back(pcl::PointXYZ(x, y, z));
+					pcl_msg.push_back(pcl::PointXYZ(x, y, height_));
 				}
 			}
 		}
