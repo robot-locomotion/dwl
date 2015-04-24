9317e8c453bfab8658da48216a3b2082aa0bef29
Carlos Mastalli <carlos.mastalli@iit.it>
2015-04-24 09:35:13 +0200

    Added Stance Posture feature for the Footstep planner.

---
+++ include/environment/StancePostureFeature.h
@@ -0,0 +1,43 @@
+#ifndef DWL_StancePostureFeature_H
+#define DWL_StancePostureFeature_H
+
+#include <environment/Feature.h>
+#include <utils/utils.h>
+
+
+namespace dwl
+{
+
+namespace environment
+{
+
+/**
+ * @class StancePostureFeature
+ * @brief Class for computing the reward value of stance posture feature
+ */
+class StancePostureFeature : public Feature
+{
+	public:
+		/** @brief Constructor function */
+		StancePostureFeature();
+
+		/** @brief Destructor function */
+		~StancePostureFeature();
+
+		/**
+		 * @brief Compute the reward value given a robot and terrain information
+		 * @param double& Reward value
+		 * @param RobotAndTerrain Information of the robot and terrain
+		 */
+		void computeReward(double& reward_value, RobotAndTerrain info);
+
+
+	private:
+		double max_distance_;
+		bool first_time_;
+};
+
+} //@namespace environment
+} //@namespace dwl
+
+#endif
