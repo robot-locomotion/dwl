28087ece9144c862d36dc8df8264df337d4b9b42
Carlos Mastalli <carlos.mastalli@iit.it>
2015-05-30 18:13:25 +0200

    [wip] Added the methods for the full dynamic constraint

--- include/model/FullDynamicSystem.h
+++ include/model/FullDynamicSystem.h
@@ -11,11 +11,26 @@ namespace model
 class FullDynamicSystem : public Constraint
 {
 	public:
+		/** @brief Constructor function */
 		FullDynamicSystem();
+
+		/** @brief Destructor function */
 		~FullDynamicSystem();
 
-		void compute(Eigen::VectorXd& constraint, const Eigen::Ref<const Eigen::VectorXd>& state);
+		/**
+		 * @brief Computes the full dynamic constraint vector given a certain state
+		 * @param Eigen::VectorXd& Evaluated constraint function
+		 * @param const StateModel& State vector
+		 */
+		void compute(Eigen::VectorXd& constraint, const StateModel& state);
 
+		/**
+		 * @brief Computes the Jacobian of the full dynamic constraint given a certain state
+		 * @param Eigen::MatrixXd& Jacobian of the constraint function
+		 * @param const StateModel& State vector
+		 */
+		void computeJacobian(Eigen::MatrixXd& jacobian,
+							 const StateModel& state);
 };
 
 } //@namespace model
