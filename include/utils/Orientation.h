aa0976914174db64a0c1355b1406f97acb74b6a4
Carlos Mastalli <carlos.mastalli@iit.it>
2015-06-04 16:09:02 +0200

    Added more documentation to orientation methods

--- include/utils/Orientation.h
+++ include/utils/Orientation.h
@@ -56,9 +56,25 @@ Eigen::Matrix3d getRotationMatrix(const Eigen::Quaterniond& quaternion);
  */
 Eigen::Matrix3d getRotationMatrix(const Eigen::Vector3d& rpy);
 
-
+/**
+ * @brief Gets the roll angle
+ * @param const Eigen::Vector3d& Roll, pitch and yaw vector
+ * @return double Roll angle
+ */
 double getRoll(const Eigen::Vector3d& rpy);
+
+/**
+ * @brief Gets the pitch angle
+ * @param const Eigen::Vector3d& Roll, pitch and yaw vector
+ * @return double Pitch angle
+ */
 double getPitch(const Eigen::Vector3d& rpy);
+
+/**
+ * @brief Gets the yaw angle
+ * @param const Eigen::Vector3d& Roll, pitch and yaw vector
+ * @return double Yaw angle
+ */
 double getYaw(const Eigen::Vector3d& rpy);
 
 }
