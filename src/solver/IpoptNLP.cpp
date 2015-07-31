21f548c19e371b79374cc9a8f3528f0b3b673f90
Carlos Mastalli <carlos.mastalli@iit.it>
2015-07-31 15:33:25 +0200

    Cleaned the IpoptNLP solver

--- src/solver/IpoptNLP.cpp
+++ src/solver/IpoptNLP.cpp
@@ -22,55 +22,23 @@ IpoptNLP::~IpoptNLP()
 
 bool IpoptNLP::init()
 {
-	// Create a new instance of your nlp
+	// Create a new instance of your NLP
 	nlp_ptr_ = &ipopt_;//new IpoptWrapper();
 
-	// Create a new instance of IpoptApplication
-	//  (use a SmartPtr, not raw)
-	// We are using the factory, since this allows us to compile this
-	// example with an Ipopt Windows DLL
+	// Creating a new instance of IpoptApplication (use a SmartPtr, not raw). We are using the
+	// factory, since this allows us to compile this example with an Ipopt Windows DLL
 	app_ = IpoptApplicationFactory();
 	app_->RethrowNonIpoptException(true);
 
 	// Change some options (do not touch these)
 	app_->Options()->SetNumericValue("tol", 1e-7);
 	app_->Options()->SetStringValue("mu_strategy", "adaptive");
-	app_->Options()->SetStringValue("output_file", "ipopt.out");
+//	app_->Options()->SetStringValue("output_file", "ipopt.out");
 	app_->Options()->SetIntegerValue("max_iter", std::numeric_limits<int>::max());
 
-	//app->Options()->SetStringValue("print_user_options","yes");
-//	app_->Options()->SetStringValue("check_derivatives_for_naninf","yes");
-	//print level
-//	if  (posOpt->log_->isDebugEnabled())
-//		app->Options()->SetIntegerValue("print_level", (int)5);//verbosity The larger this value the more detailed is the output.The valid range for this integer option is 0<print level<12
-//	else
-//		app->Options()->SetIntegerValue("print_level", (int)0);
-
-	// Compute derivatives numerically
-	// Compute Jacobian numerically put return true in eval_grad and eval_jac_g
-//	app_->Options()->SetStringValue("jacobian_approximation", "finite-difference-values");
-	// compute Hessian numerically (do not need to implement)
+	// Computing Hessian numerically (do not need to implement)
 	app_->Options()->SetStringValue("hessian_approximation", "limited-memory");
-
 //	app_->Options()->SetStringValue("warm_start_init_point", "yes");
-
-	//test the derivatives
-	//app->Options()->SetStringValue("derivative_test","first-order");//first-order = check jacobians, second-order = check hessian
-	//app->Options()->SetStringValue("derivative_test_print_all","no");//no: Print only suspect derivatives, yes: Print all derivatives see the user-provided and estimated derivative values with
-	//the relative deviation for each single partial derivative,
-	//app->Options()->SetNumericValue("derivative_test_tol", 1); //inf< <0.00001
-	//app->Options()->SetNumericValue("derivative_test_perturbation",1e-6);
-	//derivative checker
-	//* grad_f[          2] = -6.5159999999999991e+02    ~ -6.5559997134793468e+02  [ 6.101e-03]
-	//
-	//The star (``*'') in the first column indicates that this line corresponds to some partial
-	//derivative for which the error tolerance was exceeded. Next, we see which partial derivative
-	//is concerned in this output line. For example, in the first line, it is the second component
-	//of the objective function gradient (or the third, if the C_STYLE numbering is used, i.e., when
-	//counting of indices starts with 0 instead of 1). The first floating point number is the value
-	//given by the user code, and the second number (after ``~'') is the finite differences estimation.
-	//Finally, the number in square brackets is the relative difference between these two numbers.
-
 
 	// Initialize the IpoptApplication and process the options
 	Ipopt::ApplicationReturnStatus status;
