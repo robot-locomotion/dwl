866d5a61b4f17323eabf80b2c4c091a01d444592
Carlos Mastalli <carlos.mastalli@iit.it>
2015-08-03 21:56:55 +0200

    Updated the constrained whole-body planner with the new WholeBodyTrajectoryOptimiza class

--- include/dwl_planners/ConstrainedWholeBodyPlanner.h
+++ include/dwl_planners/ConstrainedWholeBodyPlanner.h
@@ -4,6 +4,7 @@
 #include <ros/ros.h>
 
 // Locomotion headers
+#include <locomotion/WholeBodyTrajectoryOptimization.h>
 #include <model/ConstrainedDynamicalSystem.h>
 #include <model/StateTrackingEnergyCost.h>
 #include <model/ControlEnergyCost.h>
@@ -19,29 +20,44 @@ namespace dwl_planners
 class ConstrainedWholeBodyPlanner
 {
 	public:
+		/** @brief Constructor function */
 		ConstrainedWholeBodyPlanner();
+
+		/** @brief Destructor function */
 		~ConstrainedWholeBodyPlanner();
 
+		/** @brief Initializes the constrained whole-body planner */
 		void init();
+
+		/** @brief Computes the whole-body trajectory */
 		bool compute();
 
+		/** @brief Publishes the planned whole-body trajectory */
 		void publishWholeBodyTrajectory();
 
 
 	private:
+		/** @brief Ros node handle */
 		ros::NodeHandle node_;
+
+		/** Motion plan publisher */
 		ros::Publisher motion_plan_pub_;
 
-		dwl::solver::OptimizationSolver* solver_;
-		dwl::solver::IpoptNLP* ipopt_solver_;
-		dwl::model::ConstrainedDynamicalSystem* constrained_system_;
+		/** @brief Whole-body trajectory optimization */
+		dwl::locomotion::WholeBodyTrajectoryOptimization planning_;
+
+		/** @brief Floating-base system information */
 		dwl::rbd::FloatingBaseSystem floating_base_system_;
 
+		/** @brief Current whole-body state */
 		dwl::LocomotionState current_state_;
+
+		/** @brief Desired whole-body state */
+		dwl::LocomotionState desired_state_;
 		dwl::LocomotionState lower_bound_;
 		dwl::LocomotionState upper_bound_;
 
-
+		/** @brief Whole-body trajectory message */
 		dwl_planners::WholeBodyTrajectory robot_trajectory_msg_;
 };
 
--- src/ConstrainedWholeBodyPlanner.cpp
+++ src/ConstrainedWholeBodyPlanner.cpp
@@ -4,18 +4,18 @@
 namespace dwl_planners
 {
 
-ConstrainedWholeBodyPlanner::ConstrainedWholeBodyPlanner() : solver_(NULL)
+ConstrainedWholeBodyPlanner::ConstrainedWholeBodyPlanner()
 {
-	ipopt_solver_ = new dwl::solver::IpoptNLP();
-	constrained_system_ = new dwl::model::ConstrainedDynamicalSystem();
-
-
+	desired_state_.base_pos << 0, 0, 0, 0, 0, -0.1;
 	current_state_.joint_pos = Eigen::VectorXd::Zero(2);
 	current_state_.joint_pos << 0.6, -1.5;
 	current_state_.joint_vel = Eigen::VectorXd::Zero(2);
 	current_state_.joint_acc = Eigen::VectorXd::Zero(2);
 	current_state_.joint_eff = Eigen::VectorXd::Zero(2);
 	current_state_.joint_eff << 9.33031, 27.6003;
+
+	floating_base_system_.LZ.active = true;
+	floating_base_system_.LZ.id = 0;
 }
 
 
@@ -27,27 +27,28 @@ ConstrainedWholeBodyPlanner::~ConstrainedWholeBodyPlanner()
 
 void ConstrainedWholeBodyPlanner::init()
 {
-	solver_ = ipopt_solver_;
+	// Initializes Ipopt solver
+	dwl::solver::IpoptNLP* ipopt_solver = new dwl::solver::IpoptNLP();
+	dwl::solver::OptimizationSolver* solver = ipopt_solver;
+
+	planning_.init(solver);
 
 
-
-
-	floating_base_system_.LZ.active = true;
-	floating_base_system_.LZ.id = 0;
+	// Initializes the dynamical system constraint
+	std::string model_file = "/home/cmastalli/ros_workspace/src/dwl/thirdparty/rbdl/hyl.urdf";
+	dwl::model::ConstrainedDynamicalSystem* system = new dwl::model::ConstrainedDynamicalSystem();
+	dwl::model::DynamicalSystem* dynamical_system = system;
 
 
 	dwl::rbd::BodySelector active_contact;
 	active_contact.push_back("foot");
-	constrained_system_->setActiveEndEffectors(active_contact);
+	system->setActiveEndEffectors(active_contact);
 
-
-	std::string model_file = "/home/cmastalli/ros_workspace/src/dwl/thirdparty/rbdl/hyl.urdf";
-	dwl::model::DynamicalSystem* dynamical_system = constrained_system_;
 	dynamical_system->modelFromURDFFile(model_file, &floating_base_system_, true);
 
 
-	dwl::LocomotionState desired_state;
-	desired_state.base_pos << 0, 0, 0, 0, 0, -0.1;
+
+
 
 
 	dwl::LocomotionState weights;
@@ -57,46 +58,48 @@ void ConstrainedWholeBodyPlanner::init()
 
 
 	dwl::model::Cost* state_tracking_cost = new dwl::model::StateTrackingEnergyCost();
-	state_tracking_cost->setDesiredState(desired_state);
 	state_tracking_cost->setWeights(weights);
 	dwl::model::Cost* control_cost = new dwl::model::ControlEnergyCost();
 	control_cost->setWeights(weights);
-	constrained_system_->setStartingState(current_state_);
 
 
-//	ipopt_solver_->getIpopt().getOptimizationModel().addDynamicalSystem(dynamical_system);
-//	ipopt_solver_->getIpopt().getOptimizationModel().addCost(state_tracking_cost);
-//	ipopt_solver_->getIpopt().getOptimizationModel().addCost(control_cost);
-	solver_->getOptimizationModel().addDynamicalSystem(dynamical_system);
-	solver_->getOptimizationModel().addCost(state_tracking_cost);
-	solver_->getOptimizationModel().addCost(control_cost);
+	planning_.addDynamicalSystem(dynamical_system);
+	planning_.addCost(state_tracking_cost);
+	planning_.addCost(control_cost);
 
 
-//	ipopt_solver_->getIpopt().getOptimizationModel().getDynamicalSystem()->setInitialState(current_state_);
-//	ipopt_solver_->getIpopt().getOptimizationModel().setHorizon(3);
-	solver_->getOptimizationModel().getDynamicalSystem()->setInitialState(current_state_);
-	solver_->getOptimizationModel().setHorizon(3);
-
-//	planning_ptr_->reset(&robot_, solver_, environment_);
-
-
-	solver_->init();
+	planning_.setHorizon(3);
 }
 
 
 bool ConstrainedWholeBodyPlanner::compute()
 {
-	return solver_->compute();
+	return planning_.compute(current_state_, desired_state_, 1000);
 }
 
 
 void ConstrainedWholeBodyPlanner::publishWholeBodyTrajectory()
 {
 	// Publishing the motion plan if there is at least one subscriber
-	if (motion_plan_pub_.getNumSubscribers() > 0) {
+//	if (motion_plan_pub_.getNumSubscribers() > 0) {
 		robot_trajectory_msg_.header.stamp = ros::Time::now();
-		robot_trajectory_msg_.trajectory.resize(1);
-	}
+
+		std::vector<dwl::LocomotionState> trajectory = planning_.getWholeBodyTrajectory();
+
+		for (unsigned int i = 0; i < trajectory.size(); i++) {
+			std::cout << "\n\nPoint = " << i << std::endl;
+			std::cout << "base_pos = " << trajectory[i].base_pos.transpose() << std::endl;
+			std::cout << "joint_pos = " << trajectory[i].joint_pos.transpose() << std::endl;
+			std::cout << "base_vel = " << trajectory[i].base_vel.transpose() << std::endl;
+			std::cout << "joint_vel = " << trajectory[i].joint_vel.transpose() << std::endl;
+			std::cout << "base_acc = " << trajectory[i].base_acc.transpose() << std::endl;
+			std::cout << "joint_acc = " << trajectory[i].joint_acc.transpose() << std::endl;
+			std::cout << "base_eff = " << trajectory[i].base_eff.transpose() << std::endl;
+			std::cout << "joint_eff = " << trajectory[i].joint_eff.transpose() << std::endl;
+		}
+
+		robot_trajectory_msg_.trajectory.resize(trajectory.size());
+//	}
 }
 
 } //@namespace dwl_planners
