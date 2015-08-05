5b83c017183945c15ba98a487a4020d3276b9468
Carlos Mastalli <carlos.mastalli@iit.it>
2015-08-05 16:14:47 +0200

    Decoupled urdf methods from rigid body dynamic methods

--- include/utils/URDF.h
+++ include/utils/URDF.h
@@ -1,9 +1,9 @@
 #ifndef DWL__URDF_MODEL__H
 #define DWL__URDF_MODEL__H
 
-#include <utils/RigidBodyDynamics.h>
 #include <urdf_model/model.h>
 #include <urdf_parser/urdf_parser.h>
+#include <Eigen/Dense>
 #include <map>
 #include <stack>
 
@@ -14,29 +14,28 @@ namespace dwl
 namespace urdf_model
 {
 
-typedef rbd::BodyID JointID;
+typedef std::map<std::string,unsigned int> JointID;
+typedef std::vector<std::string> LinkSelector;
+enum JointType {free=0, fixed, both};
+
 
 /**
- * @brief Get the joint names from URDF model. Floating-base joint are not considered as joints
+ * @brief Gets the joint names from URDF model. By default free joints are get but it's possible
+ * to get free or both joints
  * @param JointID& Joint ids and names
  * @param std::string URDF model
- * @param struct FloatingBaseSystem* Defines the general properties of a floating-base
- * system
  */
 void getJointNames(JointID& joints,
 				   std::string urdf_model,
-				   rbd::FloatingBaseSystem* system = NULL);
+				   enum JointType type = free);
 
 /**
  * @brief Get the end-effector names from URDF model
- * @param rbd::BodySelector& End-effector names
+ * @param LinkSelector& End-effector link names
  * @param std::string URDF model
- * @param struct FloatingBaseSystem* Defines the general properties of a floating-base
- * system
  */
-void getEndEffectors(rbd::BodySelector& end_effectors,
-					 std::string urdf_model,
-					 rbd::FloatingBaseSystem* system);
+void getEndEffectors(LinkSelector& end_effectors,
+					 std::string urdf_model);
 
 /**
  * @brief Get the joint limits from URDF model. Floating-base joint are not considered as joints
@@ -44,15 +43,12 @@ void getEndEffectors(rbd::BodySelector& end_effectors,
  * @param Eigen::VectorXd& Upper joint position limits
  * @param Eigen::VectorXd& Joint velocity limits
  * @param Eigen::VectorXd& Joint effort limits
- * @param struct FloatingBaseSystem* Defines the general properties of a floating-base
- * system
  */
 void getJointLimits(Eigen::VectorXd& lower_joint_pos,
 					Eigen::VectorXd& upper_joint_pos,
 					Eigen::VectorXd& joint_vel,
 					Eigen::VectorXd& joint_eff,
-					std::string urdf_model,
-					rbd::FloatingBaseSystem* system);
+					std::string urdf_model);
 
 } //@namespace urdf_model
 } //@namespace dwl
--- src/utils/URDF.cpp
+++ src/utils/URDF.cpp
@@ -9,7 +9,7 @@ namespace urdf_model
 
 void getJointNames(JointID& joints,
 				   std::string urdf_model,
-				   rbd::FloatingBaseSystem* system)
+				   enum JointType type)
 {
 	// Parsing the URDF-XML
 	boost::shared_ptr<urdf::ModelInterface> model = urdf::parseURDF(urdf_model);
@@ -25,7 +25,7 @@ void getJointNames(JointID& joints,
 		branch_index_stack.push(0);
 	}
 
-	unsigned joint_idx = 0, virtual_joints_idx = 0;
+	unsigned joint_idx = 0;
 	while (link_stack.size() > 0) {
 		boost::shared_ptr<urdf::Link> current_link = link_stack.top();
 		unsigned int branch_idx = branch_index_stack.top();
@@ -40,19 +40,28 @@ void getJointNames(JointID& joints,
 			link_stack.push(link_map[current_joint->child_link_name]);
 			branch_index_stack.push(0);
 
-			if (current_joint->type == urdf::Joint::PRISMATIC ||
-					current_joint->type == urdf::Joint::REVOLUTE ||
-					current_joint->type == urdf::Joint::CONTINUOUS) {
-				if (system != NULL &&
-						system->getTypeOfDynamicSystem() == rbd::VirtualFloatingBase &&
-						system->getFloatingBaseDOF() != virtual_joints_idx) {
-					virtual_joints_idx++;
-				} else {
+			// Searching joints names
+			if (type == free) {
+				if (current_joint->type == urdf::Joint::FLOATING ||
+						current_joint->type == urdf::Joint::PRISMATIC ||
+						current_joint->type == urdf::Joint::REVOLUTE ||
+						current_joint->type == urdf::Joint::CONTINUOUS) {
 					joints[current_joint->name] = joint_idx;
 					joint_idx++;
 				}
-			} else if (current_joint->type == urdf::Joint::FIXED)
-				joints[current_joint->name] = std::numeric_limits<unsigned int>::max();
+			} else if (type == fixed) {
+				if (current_joint->type == urdf::Joint::FIXED) {
+					joints[current_joint->name] = joint_idx;
+					joint_idx++;
+				}
+			} else { // both joints
+				if (current_joint->type == urdf::Joint::FIXED)
+					joints[current_joint->name] = std::numeric_limits<unsigned int>::max();
+				else {
+					joints[current_joint->name] = joint_idx;
+					joint_idx++;
+				}
+			}
 		} else {
 			link_stack.pop();
 			branch_index_stack.pop();
@@ -61,41 +70,41 @@ void getJointNames(JointID& joints,
 }
 
 
-void getEndEffectors(rbd::BodySelector& end_effectors,
-					 std::string urdf_model,
-					 rbd::FloatingBaseSystem* system)
+void getEndEffectors(LinkSelector& end_effectors,
+					 std::string urdf_model)
 {
 	// Parsing the URDF-XML
 	boost::shared_ptr<urdf::ModelInterface> model = urdf::parseURDF(urdf_model);
 
 	// Getting the joint names
-	JointID joints;
-	getJointNames(joints, urdf_model, system);
+	JointID fixed_joints;
+	getJointNames(fixed_joints, urdf_model, fixed);
+
+	// Getting the world, root, parent and child links
+	boost::shared_ptr<urdf::Link> world_link = model->links_[model->getRoot()->name];
+	boost::shared_ptr<urdf::Link> root_link = world_link->child_links[0];
 
 	// Searching the end-effector joints
-	for (urdf_model::JointID::iterator jnt_it = joints.begin();
-			jnt_it != joints.end(); jnt_it++) {
+	for (urdf_model::JointID::iterator jnt_it = fixed_joints.begin();
+			jnt_it != fixed_joints.end(); jnt_it++) {
 		std::string joint_name = jnt_it->first;
 		boost::shared_ptr<urdf::Joint> current_joint = model->joints_[joint_name];
 
+		// Getting the parent and child link of the current fixed joint
+		boost::shared_ptr<urdf::Link> parent_link = model->links_[current_joint->parent_link_name];
+		boost::shared_ptr<urdf::Link> child_link = model->links_[current_joint->child_link_name];
+
 		// Checking if it's an end-effector
-		if (current_joint->type == urdf::Joint::FIXED) {
-			boost::shared_ptr<urdf::Link> root_link = model->links_[model->getRoot()->name];
-			boost::shared_ptr<urdf::Link> parent_link = model->links_[current_joint->parent_link_name];
-			boost::shared_ptr<urdf::Link> child_link = model->links_[current_joint->child_link_name];
-
-			unsigned int num_childs = child_link->child_joints.size();
-			while (parent_link->name != root_link->name && num_childs == 0) {
-				boost::shared_ptr<urdf::Joint> parent_joint = model->joints_[parent_link->parent_joint->name];
-
-				if (parent_joint->type == urdf::Joint::PRISMATIC ||
-						current_joint->type == urdf::Joint::REVOLUTE) {
-					end_effectors.push_back(current_joint->child_link_name);
-					break;
-				}
-
-				parent_link = parent_link->getParent();
+		unsigned int num_childs = child_link->child_joints.size();
+		while (parent_link->name != root_link->name && num_childs == 0) {
+			boost::shared_ptr<urdf::Joint> parent_joint = model->joints_[parent_link->parent_joint->name];
+			if (parent_joint->type == urdf::Joint::PRISMATIC ||
+					parent_joint->type == urdf::Joint::REVOLUTE) {
+				end_effectors.push_back(current_joint->child_link_name);
+				break;
 			}
+
+			parent_link = parent_link->getParent();
 		}
 	}
 }
@@ -105,15 +114,14 @@ void getJointLimits(Eigen::VectorXd& lower_joint_pos,
 					Eigen::VectorXd& upper_joint_pos,
 					Eigen::VectorXd& joint_vel,
 					Eigen::VectorXd& joint_eff,
-					std::string urdf_model,
-					rbd::FloatingBaseSystem* system)
+					std::string urdf_model)
 {
 	// Parsing the URDF-XML
 	boost::shared_ptr<urdf::ModelInterface> model = urdf::parseURDF(urdf_model);
 
 	// Getting the joint names
 	JointID joints;
-	getJointNames(joints, urdf_model, system);
+	getJointNames(joints, urdf_model, free);
 
 	// Computing the number of actuated joints
 	unsigned num_joints = 0;
@@ -122,9 +130,9 @@ void getJointLimits(Eigen::VectorXd& lower_joint_pos,
 		std::string joint_name = jnt_it->first;
 		boost::shared_ptr<urdf::Joint> current_joint = model->joints_[joint_name];
 
-		if (current_joint->type == urdf::Joint::PRISMATIC ||
-				current_joint->type == urdf::Joint::REVOLUTE)
-			num_joints++;
+		if (current_joint->type != urdf::Joint::FLOATING)
+			if (current_joint->limits->effort != 0) // Virtual floating-base joints
+				num_joints++;
 	}
 
 	// Resizing the vectors
@@ -134,18 +142,20 @@ void getJointLimits(Eigen::VectorXd& lower_joint_pos,
 	joint_eff.resize(num_joints);
 
 	// Searching the joint limits
+	unsigned int joint_idx = 0;
 	for (urdf_model::JointID::iterator jnt_it = joints.begin();
 			jnt_it != joints.end(); jnt_it++) {
 		std::string joint_name = jnt_it->first;
 		boost::shared_ptr<urdf::Joint> current_joint = model->joints_[joint_name];
 
-		if (current_joint->type == urdf::Joint::PRISMATIC ||
-				current_joint->type == urdf::Joint::REVOLUTE) {
-			unsigned int joint_idx = jnt_it->second;
-			lower_joint_pos(joint_idx) = current_joint->limits->lower;
-			upper_joint_pos(joint_idx) = current_joint->limits->upper;
-			joint_vel(joint_idx) = current_joint->limits->velocity;
-			joint_eff(joint_idx) = current_joint->limits->effort;
+		if (current_joint->type != urdf::Joint::FLOATING) {
+			if (current_joint->limits->effort != 0) {  // Virtual floating-base joints
+				lower_joint_pos(joint_idx) = current_joint->limits->lower;
+				upper_joint_pos(joint_idx) = current_joint->limits->upper;
+				joint_vel(joint_idx) = current_joint->limits->velocity;
+				joint_eff(joint_idx) = current_joint->limits->effort;
+				joint_idx++;
+			}
 		}
 	}
 }
