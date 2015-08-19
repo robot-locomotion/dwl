224f9b304143606177a9dd0cc18dfbffb1425de3
Carlos Mastalli <carlos.mastalli@iit.it>
2015-08-19 17:12:22 +0200

    Added another complementary constraint for the velocity of the contacts

---
+++ src/model/InelasticContactVelocityConstraint.cpp
@@ -0,0 +1,73 @@
+#include <model/InelasticContactVelocityConstraint.h>
+
+
+namespace dwl
+{
+
+namespace model
+{
+
+InelasticContactVelocityConstraint::InelasticContactVelocityConstraint()
+{
+	// Setting the name of the constraint
+	name_ = "inelastic contact velocity model";
+}
+
+
+InelasticContactVelocityConstraint::~InelasticContactVelocityConstraint()
+{
+
+}
+
+
+void InelasticContactVelocityConstraint::init(std::string urdf_model,
+										   bool info)
+{
+	// Getting the end-effector names
+	end_effector_names_.clear();
+	urdf_model::LinkID end_effector = system_.getEndEffectors();
+	for (urdf_model::LinkID::iterator endeffector_it = end_effector.begin();
+			endeffector_it != end_effector.end(); endeffector_it++) {
+		// Getting and setting the end-effector names
+		std::string name = endeffector_it->first;
+		end_effector_names_.push_back(name);
+	}
+
+	// Setting the complementary dimension
+	complementary_dimension_ = system_.getNumberOfEndEffectors();
+}
+
+
+void InelasticContactVelocityConstraint::computeFirstComplement(Eigen::VectorXd& constraint,
+																const LocomotionState& state)
+{
+	// Resizing the complementary constraint dimension
+	constraint.resize(system_.getNumberOfEndEffectors());
+
+	// Adding the normal contact forces per every end-effector as a the first complementary
+	for (unsigned int k = 0; k < system_.getNumberOfEndEffectors(); k++)
+		constraint(k) = state.contacts[k].force(rbd::Z);
+}
+
+
+void InelasticContactVelocityConstraint::computeSecondComplement(Eigen::VectorXd& constraint,
+																 const LocomotionState& state)
+{
+	// Resizing the complementary constraint dimension
+	constraint.resize(system_.getNumberOfEndEffectors());
+
+	// Computing the contact velocity
+	rbd::BodyVector contact_vel;
+	kinematics_.computeVelocity(contact_vel,
+								state.base_pos, state.joint_pos,
+								state.base_vel, state.joint_vel,
+								end_effector_names_, rbd::Linear);
+
+	// Adding the contact distance per every end-effector as a the second complementary
+	// TODO there is missing the concept of surface
+	for (unsigned int k = 0; k < system_.getNumberOfEndEffectors(); k++)
+		constraint(k) = contact_vel.find(end_effector_names_[k])->second(rbd::X);
+}
+
+} //@namespace model
+} //@namespace dwl
