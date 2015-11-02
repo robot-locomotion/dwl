031527cee6f4f3e6830ae381390799ae597aac41
Carlos Mastalli <carlos.mastalli@iit.it>
2015-11-02 22:43:47 +0100

    Added the standard model predictive control files from my old mpc project

---
+++ dwl/include/dwl/locomotion/ModelPredictiveControl.h
@@ -0,0 +1,178 @@
+#ifndef DWL__LOCOMOTION__MODEL_PREDICTIVE_CONTROL__H
+#define DWL__LOCOMOTION__MODEL_PREDICTIVE_CONTROL__H
+
+#include <dwl/model/LinearModel.h>
+#include <dwl/solver/QuadraticProgram.h>
+
+#include <Eigen/Dense>
+
+#include <fstream>
+
+
+namespace dwl
+{
+
+namespace locomotion
+{
+
+/**
+ @class ModelPredictiveControl
+ @brief This class serves as a base class in order to expand the functionality of the library and
+ implement different sorts of MPC algorithms. The methods defined here are conceived in the simplest
+ way possible to allow different implementations in the derived classes.\n
+*/
+class ModelPredictiveControl
+{
+	public:
+		/** @brief Constructor function */
+		ModelPredictiveControl();
+
+		/** @brief Destructor function */
+		~ModelPredictiveControl();
+
+		/**
+		 @brief Function to specify and set the settings of all the components within the MPC
+		 problem. The mpc::ModelPredictiveControl class can change individual parts of the MPC
+		 problem; such as the model (mpc::model::Model and derived classes), the optimizer
+		 (mpc::optimizer::Optimizer and derived classes) and, if used, the plant simulator
+		 (mpc::model::Simulator and derived classes) in order to allow different combinations of
+		 these parts when solving.
+		 @param mpc::model::Model Pointer to the model of the plant to be used in the algorithm
+		 @param mpc::optimizer::Optimizer Pointer to the optimization library to be used in the
+		 algorithm
+		 */
+		virtual bool reset(dwl::model::LinearModel* model,
+						   dwl::solver::QuadraticProgram* optimizer) = 0;
+
+		/**
+		 @brief Function to initialize the calculation of the MPC algorithm. The function reads
+		 all required parameters from ROS' parameter server that has been previously loaded from
+		 a configuration YAML file, and performs all the initial calculations of variables to be
+		 used in the optimization problem.
+		 @return Label that indicates if the MPC is initialized with success
+		 */
+		virtual bool init() = 0;
+
+		/**
+		  @brief Function to update the MPC algorithm for the next iteration. The parameters
+		  defined and calculated in dwl::solver::ModelPredictiveControl::init() are used together
+		  with the methods taken from the MPC class components (mpc::model::Model,
+		  mpc::optimizer::Optimizer and mpc::model::Simulator) to find a solution to the
+		  optimization problem. This is where the different variants of MPC algorithms can be
+		  implemented in a source file from a derived class.
+		  @param double* State vector
+		  @param double* Reference vector
+		 */
+		virtual void update(double* x_measured,
+							double* x_reference) = 0;
+			
+		/**
+		 @brief Function to get the control signal generated for the MPC. As the MPC algorithm
+		 states, the optimization process yields the control signals for a range of times defined
+		 by the prediction horizon, but only the current control signal is applied to the plant.
+		 This function returns the control signal for the current time.
+		 @return double* Control signal for the current MPC iteration.
+		 */
+		virtual double* getControlSignal() const;
+
+			
+	protected:
+		/** @brief Pointer of dynamic model of the system */
+		mpc::model::Model *model_;
+			
+		/** @brief Pointer of the simulator of the system */
+		mpc::model::Simulator *simulator_;
+			
+		/** @brief Pointer of the optimizer of the MPC */
+		mpc::optimizer::Optimizer *optimizer_;
+			
+		/** @brief Number of states of the dynamic model */
+		int states_;
+			
+		/** @brief Number of inputs of the dynamic model */
+		int inputs_;
+			
+		/** @brief Number of outputs of the dynamic model */
+		int outputs_;
+			
+		/** @brief Horizon of prediction of the dynamic model */
+		int horizon_;
+			
+		/** @brief Number of variables, i.e inputs * horizon */
+		int variables_;
+			
+		/** @brief Number of constraints */
+		int constraints_;
+
+		/** @brief Vector of the operation points for the states in case of a LTI model */
+		double* operation_states_;
+
+		/** @brief Vector of the operation points for the inputs in case of a LTI model */
+		double* operation_inputs_;
+			
+		/** @brief Infeasibility counter in the solution */
+		int infeasibility_counter_;
+			
+		/** @brief Maximun value of the infeasibility counter */
+		int infeasibility_hack_counter_max_;
+			
+		/** @brief States error weight matrix */
+		Eigen::MatrixXd Q_;
+			
+		/** @brief Terminal states error weight matrix */
+		Eigen::MatrixXd P_;
+			
+		/** @brief Input error weight matrix */
+		Eigen::MatrixXd R_;
+			
+		/** @brief Vector of the MPC solution */
+		double *mpc_solution_;
+			
+		/** @brief Stationary control signal for the reference state vector */
+		Eigen::MatrixXd u_reference_;
+			
+		/** @brief Control signal computes for MPC */
+		double *control_signal_;
+			
+		/** @brief Data of the time vector of the system */
+		std::vector<int> t_;
+			
+		/** @brief Data of the state vector of the system */
+		std::vector<std::vector<double> > x_;
+			
+		/** @brief Data of the reference state vector of the system */
+		std::vector<std::vector<double> > xref_;
+			
+		/** @brief Data of the control signal vector of the system */
+		std::vector<std::vector<double> > u_;
+			
+		/** @brief Path where the data will be save */
+		std::string path_name_;
+			
+		/** @brief Name of the file where the data will be save */
+		std::string data_name_;
+			
+		/** @brief Label that indicates if it will be save the data */
+		bool enable_record_;
+
+
+		private:
+};
+
+} //@namespace locomotion
+} //@namespace dwl
+
+
+inline double* dwl::locomotion::ModelPredictiveControl::getControlSignal() const
+{
+	for (int i = 0; i < inputs_; i++) {
+		if (infeasibility_counter_ < infeasibility_hack_counter_max_)
+			control_signal_[i] = mpc_solution_[infeasibility_counter_ * inputs_ + i];
+		else
+			control_signal_[i] = u_reference_(i);
+	}
+
+	return control_signal_;
+}
+
+#endif
---
+++ dwl/src/locomotion/ModelPredictiveControl.cpp
@@ -0,0 +1,343 @@
+#include <dwl/locomotion/ModelPredictiveControl.h>
+
+
+
+namespace dwl
+{
+
+namespace locomotion
+{
+
+ModelPredictiveControl::ModelPredictiveControl()
+{
+	model_ = 0;
+	optimizer_ = 0;
+	simulator_ = 0;
+	enable_record_ = true;
+}
+
+
+ModelPredictiveControl::~ModelPredictiveControl()
+{
+}
+
+
+bool ModelPredictiveControl::reset(dwl::model::LinearModel* model,
+								   dwl::solver::QuadraticProgram* optimizer)
+{
+	// Setting of the pointer of the model, optimizer and simulator classes
+	model_ = model;
+	optimizer_ = optimizer;
+	simulator_ = simulator;
+	time_index_ = 0;
+	
+	// Reading of the horizon value of the model predictive control algorithm
+	nh_.param<int>("horizon", horizon_, 30);
+	ROS_INFO("Got param: horizon = %d", horizon_);
+	
+	nh_.param<int>("infeasibility_hack_counter_max", infeasibility_hack_counter_max_, 1);
+	ROS_INFO("Got param: infeasibility_hack_counter_max = %d", infeasibility_hack_counter_max_);
+	
+
+	// Reading the path and data name
+	if (!nh_.getParam("path_name", path_name_)) {
+		ROS_WARN("The data will not save because could not found path name from parameter server.");
+		enable_record_ = false;
+	}
+	if (!nh_.getParam("data_name", data_name_)) {
+		ROS_WARN("The data will not save because could not found data name from parameter server.");
+		enable_record_ = false;
+	}
+	
+	
+	// Reading of the problem variables
+	states_ = model_->getStatesNumber();
+	inputs_ = model_->getInputsNumber();
+	outputs_ = model_->getOutputsNumber();
+	
+	variables_ = horizon_ * inputs_;
+	optimizer_->setHorizon(horizon_);
+	optimizer_->setVariableNumber(variables_);
+	
+	if (!optimizer_->init()) {
+		ROS_INFO("Could not initialize the optimizer class.");
+		return false;
+	}
+	constraints_ = optimizer_->getConstraintNumber();
+	
+	
+	ROS_INFO("Reset successful. States = %d \n Inputs = %d \n Outputs = %d \n Constraints = %d \n", states_,inputs_,outputs_,constraints_);
+	return true;
+}
+
+
+
+bool ModelPredictiveControl::init()
+{
+	// Initialization of MPC solution
+	mpc_solution_ = new double[variables_];
+	control_signal_ = new double[inputs_];
+	operation_states_ = new double[states_];
+	operation_inputs_ = new double[inputs_];
+	
+	
+	u_reference_ = Eigen::MatrixXd::Zero(inputs_, 1);
+	infeasibility_counter_ = 0;
+	x_.resize(states_);
+	xref_.resize(states_);
+	u_.resize(inputs_);
+	
+	
+	// Initialization of state space matrices
+	A_ = Eigen::MatrixXd::Zero(states_, states_);
+	B_ = Eigen::MatrixXd::Zero(states_, inputs_);
+	C_ = Eigen::MatrixXd::Zero(outputs_, states_);
+	
+	// Obtention of the model parameters
+	if (model_->computeLinearSystem(A_, B_)) {
+		ROS_INFO("Model calculated successfully.");
+		std::cout << "A\n" << A_ << std::endl;
+		std::cout << "B\n" << B_ << std::endl;
+	}
+	
+	// Reading the weight matrices of the cost function
+	Q_ = Eigen::MatrixXd::Zero(states_, states_);
+	P_ = Eigen::MatrixXd::Zero(states_, states_);
+	R_ = Eigen::MatrixXd::Zero(inputs_, inputs_);
+	XmlRpc::XmlRpcValue Q_list, P_list, R_list;
+	nh_.getParam("optimizer/states_error_weight_matrix/data", Q_list);
+	ROS_ASSERT(Q_list.getType() == XmlRpc::XmlRpcValue::TypeArray);
+	ROS_ASSERT(Q_list.size() == states_ * states_);
+
+	nh_.getParam("optimizer/terminal_state_weight_matrix/data", P_list);
+	ROS_ASSERT(P_list.getType() == XmlRpc::XmlRpcValue::TypeArray);
+	ROS_ASSERT(P_list.size() == states_ * states_);
+	
+	int z = 0;
+	for (int i = 0; i < states_; i++) {
+		for (int j = 0; j < states_; j++) {
+			ROS_ASSERT(Q_list[z].getType() == XmlRpc::XmlRpcValue::TypeDouble);
+			Q_(i, j) = static_cast<double>(Q_list[z]);
+			
+			ROS_ASSERT(P_list[z].getType() == XmlRpc::XmlRpcValue::TypeDouble);
+			P_(i, j) = static_cast<double>(P_list[z]);
+			z++;
+		}
+	}
+	
+	nh_.getParam("optimizer/input_error_weight_matrix/data", R_list);
+	ROS_ASSERT(R_list.getType() == XmlRpc::XmlRpcValue::TypeArray);
+	ROS_ASSERT(R_list.size() == inputs_ * inputs_);
+	z = 0;
+	for (int i = 0; i < inputs_; i++) {
+		for (int j = 0; j < inputs_; j++) {
+			ROS_ASSERT(R_list[z].getType() == XmlRpc::XmlRpcValue::TypeDouble);
+			R_(i, j) = static_cast<double>(R_list[z]);
+			z++;
+		}
+	}
+	
+	
+	// Creation of the states and inputs weight matrices for the quadratic program
+	Q_bar_ = Eigen::MatrixXd::Zero((horizon_ + 1) * states_, (horizon_ + 1) * states_);
+	R_bar_ = Eigen::MatrixXd::Zero(horizon_ * inputs_, horizon_ * inputs_);
+	for (int i = 0; i < horizon_; i++) {
+		Q_bar_.block(i * states_, i * states_, states_, states_) = Q_;
+		R_bar_.block(i * inputs_, i * inputs_, inputs_, inputs_) = R_;
+	}
+	Q_bar_.block(horizon_ * states_, horizon_ * states_, states_, states_) = P_;
+	
+	
+	// Reading the constraint vectors
+	Eigen::VectorXd lbG = Eigen::VectorXd::Zero(constraints_);
+	Eigen::VectorXd ubG = Eigen::VectorXd::Zero(constraints_);
+	XmlRpc::XmlRpcValue lbG_list, ubG_list;
+	nh_.getParam("optimizer/constraints/constraint_vector_low", lbG_list);
+	ROS_ASSERT(lbG_list.getType() == XmlRpc::XmlRpcValue::TypeArray);
+	nh_.getParam("optimizer/constraints/constraint_vector_upp", ubG_list);
+	ROS_ASSERT(ubG_list.getType() == XmlRpc::XmlRpcValue::TypeArray);
+	if (ubG_list.size() == lbG_list.size()) {
+		for (int i = 0; i < ubG_list.size(); ++i) {
+			ROS_ASSERT(lbG_list[i].getType() == XmlRpc::XmlRpcValue::TypeDouble);
+			lbG(i) = static_cast<double>(lbG_list[i]);
+			
+			ROS_ASSERT(ubG_list[i].getType() == XmlRpc::XmlRpcValue::TypeDouble);
+			ubG(i) = static_cast<double>(ubG_list[i]);
+		}
+	
+	}
+	
+	// Reading the bound vectors
+	Eigen::VectorXd lb = Eigen::VectorXd::Zero(inputs_);
+	Eigen::VectorXd ub = Eigen::VectorXd::Zero(inputs_);
+	XmlRpc::XmlRpcValue lb_list, ub_list;
+	nh_.getParam("optimizer/constraints/bound_vector_low", lb_list);
+	ROS_ASSERT(lb_list.getType() == XmlRpc::XmlRpcValue::TypeArray);
+	nh_.getParam("optimizer/constraints/bound_vector_upp", ub_list);
+	ROS_ASSERT(ub_list.getType() == XmlRpc::XmlRpcValue::TypeArray);
+	if (ub_list.size() == lb_list.size()) {
+		for (int i = 0; i < ub_list.size(); ++i) {
+			ROS_ASSERT(lb_list[i].getType() == XmlRpc::XmlRpcValue::TypeDouble);
+			lb(i) = static_cast<double>(lb_list[i]);                              
+			
+			ROS_ASSERT(ub_list[i].getType() == XmlRpc::XmlRpcValue::TypeDouble);
+			ub(i) = static_cast<double>(ub_list[i]);
+		}
+	}
+	
+	// Reading the state bound matrix
+	Eigen::MatrixXd M = Eigen::MatrixXd::Zero(constraints_, states_);
+	XmlRpc::XmlRpcValue M_list;
+	nh_.getParam("optimizer/constraints/constraint_matrix_M", M_list);
+	ROS_ASSERT(M_list.getType() == XmlRpc::XmlRpcValue::TypeArray);
+	ROS_ASSERT(M_list.size() == constraints_ * states_);
+	
+	z = 0;
+	for (int i = 0; i < constraints_; ++i) {
+		for (int j = 0; j < states_; j++) {
+			ROS_ASSERT(M_list[i].getType() == XmlRpc::XmlRpcValue::TypeDouble);
+			M(i, j) = static_cast<double>(M_list[z]);
+			z++;
+		}
+	}
+	
+	
+	// Creation of the extended constraint and bound vector
+	lbG_bar_ = Eigen::VectorXd::Zero(constraints_ * horizon_);
+	ubG_bar_ = Eigen::VectorXd::Zero(constraints_ * horizon_);
+	lb_bar_ = Eigen::VectorXd::Zero(inputs_ * horizon_);
+	ub_bar_ = Eigen::VectorXd::Zero(inputs_ * horizon_);
+	for (int i = 0; i < horizon_; i++) {
+		lbG_bar_.block(i * constraints_, 0, constraints_, 1) = lbG;
+		ubG_bar_.block(i * constraints_, 0, constraints_, 1) = ubG;
+		lb_bar_.block(i * inputs_, 0, inputs_, 1) = lb;
+		ub_bar_.block(i * inputs_, 0, inputs_, 1) = ub;
+	}
+	
+	// Creation of the extended constraint matrix G_bar_
+	M_bar_ = Eigen::MatrixXd::Zero(constraints_ * horizon_, (horizon_ + 1) * states_);
+	for (int i = 0; i < horizon_; i++) {
+		for (int j = 0; j < horizon_ + 1; j++) {
+			if (i == j) {
+				M_bar_.block(i * constraints_, j * states_, constraints_, states_) = M;
+			}
+		}
+	}
+	
+	
+	ROS_INFO("STDMPC class successfully initialized.");
+	return true;
+}
+
+
+
+void ModelPredictiveControl::update(double* x_measured, double* x_reference)
+{
+	Eigen::Map<Eigen::VectorXd> x_measured_eigen(x_measured, states_, 1);
+	Eigen::Map<Eigen::VectorXd> x_reference_eigen(x_reference, states_, 1);
+
+	// Update of the model parameters
+	if (model_->getModelType()) {
+		model_->computeLinearSystem(A_, B_);
+	}
+	
+	// Compute steady state control based on updated system matrices
+	Eigen::JacobiSVD<Eigen::MatrixXd> SVD_B(B_, Eigen::ComputeThinU | Eigen::ComputeThinV);
+	u_reference_ = SVD_B.solve(x_reference_eigen - A_ * x_reference_eigen);
+	
+	// Creation of the base vector
+	A_pow_.push_back(Eigen::MatrixXd::Identity(states_, states_));
+	for (int i = 1; i < horizon_ + 1; i++) {
+		Eigen::MatrixXd A_pow_i = A_pow_[i-1] * A_;
+		A_pow_.push_back(A_pow_i);
+	}
+
+	// Compute the hessian matrix and gradient vector for the quadratic program	
+	A_bar_ = Eigen::MatrixXd::Zero((horizon_ + 1) * states_, states_);
+	B_bar_ = Eigen::MatrixXd::Zero((horizon_ + 1) * states_, horizon_ * inputs_);
+//	Eigen::MatrixXd H_x = Eigen::MatrixXd::Zero((horizon_ + 1) * states_, horizon_ * states_);
+	Eigen::MatrixXd x_ref_bar = Eigen::MatrixXd::Zero((horizon_ + 1) * states_, 1);
+	for (int i = 0; i < horizon_ + 1; i++) {
+		for (int j = 0; j < horizon_; j++) {
+			if (i == horizon_) {
+				if (j == 0)
+					A_bar_.block(i * states_, 0, states_, states_) = A_pow_[i];
+				if (j == 0)
+					x_ref_bar.block(i * states_, 0, states_, 1) = x_reference_eigen;
+				if (i > j) {
+					B_bar_.block(i * states_, j * inputs_, states_, inputs_) = A_pow_[i-j-1] * B_;
+//					H_x.block(i * states_, j * states_, states_, states_) = A_p_BK_pow_[i-j-1];
+				}
+			}
+			else {
+				if (j == 0) {
+					A_bar_.block(i * states_, 0, states_, states_) = A_pow_[i];
+					x_ref_bar.block(i * states_, 0, states_, 1) = x_reference_eigen;
+				}
+				if (i > j) {
+					B_bar_.block(i * states_, j * inputs_, states_, inputs_) = A_pow_[i-j-1] * B_;
+//					H_x.block(i * states_, j * states_, states_, states_) = A_p_BK_pow_[i-j-1];
+				}
+			}
+		}
+	}
+	double hessian_matrix[horizon_ * inputs_][horizon_ * inputs_];
+	double gradient_vector[horizon_ * inputs_];
+	Eigen::Map<Eigen::Matrix<double, Eigen::Dynamic, Eigen::Dynamic, Eigen::RowMajor> > H(&hessian_matrix[0][0], horizon_ * inputs_, horizon_ * inputs_);
+	Eigen::Map<Eigen::VectorXd> g(gradient_vector, horizon_ * inputs_, 1);
+	
+	// Computing the values of the Hessian matrix and Gradient vector
+	H = B_bar_.transpose() * Q_bar_ * B_bar_ + R_bar_;
+	g = B_bar_.transpose() * Q_bar_ * (A_bar_ * x_measured_eigen - x_ref_bar);
+	
+	// Transforming constraints and bounds to array
+	double lbG_bar[constraints_ * horizon_];
+	double ubG_bar[constraints_ * horizon_];
+	double lb_bar[horizon_ * inputs_];
+	double ub_bar[horizon_ * inputs_];
+	Eigen::Map<Eigen::VectorXd> lbG_bar_eigen(lbG_bar, constraints_ * horizon_, 1);
+	Eigen::Map<Eigen::VectorXd> ubG_bar_eigen(ubG_bar, constraints_ * horizon_, 1);
+	Eigen::Map<Eigen::VectorXd> lb_bar_eigen(lb_bar, inputs_ * horizon_, 1);
+	Eigen::Map<Eigen::VectorXd> ub_bar_eigen(ub_bar, inputs_ * horizon_, 1);
+	lbG_bar_eigen = lbG_bar_ - M_bar_ * A_bar_ * x_measured_eigen;
+	ubG_bar_eigen = ubG_bar_ - M_bar_ * A_bar_ * x_measured_eigen;
+	lb_bar_eigen = lb_bar_;
+	ub_bar_eigen = ub_bar_;
+
+	
+	// Mapping of the extended constraint matrix G_bar_
+	double constraint_matrix[horizon_ * constraints_][horizon_ * inputs_];
+	Eigen::Map<Eigen::MatrixXd, Eigen::RowMajor> G_bar(&constraint_matrix[0][0], constraints_ * horizon_, horizon_ * inputs_);
+	G_bar = M_bar_ * B_bar_;
+	
+	
+	double cputime = 0.008;//1.0;//NULL;
+	bool success = false;
+	success = optimizer_->computeOpt(&hessian_matrix[0][0], gradient_vector, &constraint_matrix[0][0], lb_bar, ub_bar, lbG_bar, ubG_bar, cputime);
+	if (success) {
+		mpc_solution_ = optimizer_->getOptimalSolution();
+		infeasibility_counter_ = 0;
+	}
+	else {
+		infeasibility_counter_++;
+		ROS_WARN("An optimal solution could not be obtained.");
+	}
+	
+	// Save the data of the MPC
+	for (int i = 0; i < states_; i++) {
+		x_[i].push_back(x_measured[i]);
+		xref_[i].push_back(x_reference[i]);
+	}
+	t_.push_back(time_index_);
+	time_index_++;
+
+	double *u = getControlSignal();
+	for (int i = 0; i < inputs_; i++) {
+		u_[i].push_back(u[i]);
+	}
+}
+
+} //@namespace locomotion
+} //@namespace dwl
+
+
