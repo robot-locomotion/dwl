6f9c29eae09652533a6c2e13bebcad03b66ad09c
Carlos Mastalli <carlos.mastalli@iit.it>
2015-11-06 00:19:36 +0100

    Included QuadProg inside the QuadraticProgram abstract class

--- dwl/include/dwl/solver/QuadProg++QP.h
+++ dwl/include/dwl/solver/QuadProg++QP.h
@@ -1,7 +1,7 @@
 #ifndef DWL__SOLVER__QUADPROGPP_QP__H
 #define DWL__SOLVER__QUADPROGPP_QP__H
 
-#include <dwl/solver/OptimizationSolver.h>
+#include <dwl/solver/QuadraticProgram.h>
 #include <dwl/solver/QuadProg++.h>
 #include <time.h>
 
@@ -12,7 +12,7 @@ namespace dwl
 namespace solver
 {
 
-class QuadProgQP : public OptimizationSolver
+class QuadProgQP : public QuadraticProgram
 {
 	public:
 		/** @brief Constructor function */
@@ -23,16 +23,34 @@ class QuadProgQP : public OptimizationSolver
 
 		/**
 		 * @brief Initialization of the NLP solver using Ipopt
+		 * @param unsigned int Number of variables of the QP problem
+	 	 * @param unsigned int Number of constraints of the QP problem
 		 * @return True if was initialized
 		 */
-		bool init();
+		bool init(unsigned int num_variables,
+		  	  	  unsigned int num_constraints);
 
 		/**
-		 * @brief Computes a solution of an NLP given a defined computation time
-		 * @param double Allocated computation time in seconds
-		 * @return True if it was computed a solution
+	 	 * @brief Function to compute the QP solution
+	 	 * @param const Eigen::MatrixXd& Hessian matrix
+	 	 * @param const Eigen::VectorXd Gradient vector
+	 	 * @param const Eigen::MatrixXd& Constraint matrix
+	 	 * @param const Eigen::VectorXd Low bound vector
+	 	 * @param const Eigen::VectorXd Upper bound vector
+	 	 * @param const Eigen::VectorXd Low constraint vector
+	 	 * @param const Eigen::VectorXd Upper constraint vector
+	 	 * @param double CPU-time for computing the optimization. If NULL, it provides on output
+	 	 * the actual calculation time of the optimization problem.
+	 	 * @return bool Label that indicates if the computation of the optimization is successful
 		 */
-		bool compute(double allocated_time_secs = std::numeric_limits<double>::max());
+		bool compute(const Eigen::MatrixXd& hessian,
+					 const Eigen::VectorXd& gradient,
+					 const Eigen::MatrixXd& constraint_mat,
+					 const Eigen::VectorXd& lower_bound,
+					 const Eigen::VectorXd& upper_bound,
+					 const Eigen::VectorXd& lower_constraint,
+					 const Eigen::VectorXd& upper_constraint,
+					 double cputime);
 };
 
 } //@namespace solver
--- dwl/src/solver/QuadProg++QP.cpp
+++ dwl/src/solver/QuadProg++QP.cpp
@@ -19,26 +19,99 @@ QuadProgQP::~QuadProgQP()
 }
 
 
-bool QuadProgQP::init()
+bool QuadProgQP::init(unsigned int num_variables,
+					  unsigned int num_constraints)
 {
 	return true;
 }
 
 
-bool QuadProgQP::compute(double allocated_time_secs)
+bool QuadProgQP::compute(const Eigen::MatrixXd& hessian,
+						 const Eigen::VectorXd& gradient,
+						 const Eigen::MatrixXd& constraint_mat,
+						 const Eigen::VectorXd& lower_bound,
+						 const Eigen::VectorXd& upper_bound,
+						 const Eigen::VectorXd& lower_constraint,
+						 const Eigen::VectorXd& upper_constraint,
+						 double cputime)
 {
-	// Evaluating constraints and bounds
-//	model_.evaluateConstraints();
-//	model_.evaluateBounds();
+	// Getting the number of equality and inequality constraints
+	unsigned int num_constraints = lower_constraint.size();
+	unsigned int num_eq_constraints = 0;
+	unsigned int num_ineq_constraints = 0;
+	for (unsigned int i = 0; i < num_constraints; i++) {
+		if ((upper_constraint - lower_constraint).isZero())
+			++num_eq_constraints;
+		else
+			++num_ineq_constraints;
+	}
 
+	// Getting the number of equality and inequality bounds
+	unsigned int num_bounds = lower_bound.size();
+	unsigned int num_eq_bounds = 0;
+	unsigned int num_ineq_bounds = 0;
+	for (unsigned int i = 0; i < num_constraints; i++) {
+		if ((upper_bound - lower_bound).isZero())
+			++num_eq_bounds;
+		else
+			++num_ineq_bounds;
+	}
 
-	/*
-	double cost = solve_quadprog(Eigen::MatrixXd& G, Eigen::VectorXd& g0,
-			 	 	 	  const Eigen::MatrixXd& CE, const Eigen::VectorXd& ce0,
-			 	 	 	  const Eigen::MatrixXd& CI, const Eigen::VectorXd& ci0,
-			 	 	 	  Eigen::VectorXd& x);
+	// Computing the constraint in/equality matrix and in/equality bound
+	Eigen::MatrixXd eq_constraint_mat = Eigen::MatrixXd::Zero(num_eq_constraints + num_eq_bounds);
+	Eigen::MatrixXd ineq_constraint_mat = Eigen::MatrixXd::Zero(num_ineq_constraints + num_ineq_bounds);
+	Eigen::VectorXd eq_bound = Eigen::VectorXd::Zero(num_eq_constraints);
+	Eigen::VectorXd ineq_bound = Eigen::VectorXd::Zero(num_ineq_constraints);
+	unsigned int eq_idx = 0;
+	unsigned int ineq_idx = 0;
+	for (unsigned int i = 0; i < num_constraints; i++) {
+		if ((upper_constraint - lower_constraint).isZero()) {
+			// Converting lbA = Ax to Ax -lbA = 0
+			eq_constraint_mat.row(eq_idx) = constraint_mat.row(i);
+			eq_bound(eq_idx) = -lower_constraint(i);
+			++eq_idx;
+		} else {
+			// Converting lbA <= Ax <= ubA to Ax -lbA >= 0 and -Ax + ubA >= 0
+			// Setting the equality bound component
+			ineq_constraint_mat.row(ineq_idx) = constraint_mat.row(i);
+			ineq_bound(ineq_idx) = -lower_constraint(i);
+			++ineq_idx;
 
-*/
+			// Setting the inequality bound component
+			ineq_constraint_mat.row(ineq_idx) = -constraint_mat.row(i);
+			ineq_bound(ineq_idx) = upper_constraint(i);
+			++ineq_idx;
+		}
+	}
+	for (unsigned int i = 0; i < num_bounds; i++) {
+		Eigen::VectorXd identity_row = Eigen::VectorXd::Zero(num_bounds);
+		identity_row(i) = 1;
+		if ((upper_bound - lower_bound).isZero()) {
+			// Converting lb = x to Ix - lb = 0
+			eq_constraint_mat.row(eq_idx) = identity_row;
+			eq_bound(eq_idx) = -lower_bound(i);
+			++eq_idx;
+		} else {
+			// Converting lb <= x <= up to Ix - lb >= 0 and -Ix + ub >= 0
+			// Setting the equality bound component
+			ineq_constraint_mat.row(ineq_idx) = identity_row;
+			ineq_bound(ineq_idx) = -lower_bound(i);
+			++ineq_idx;
+
+			// Setting the inequality bound component
+			ineq_constraint_mat.row(ineq_idx) = -identity_row;
+			ineq_bound(ineq_idx) = upper_bound(i);
+			++ineq_idx;
+		}
+	}
+
+	Eigen::MatrixXd copy_hessian = hessian;
+	Eigen::VectorXd copy_gradient = gradient;
+	double cost = solve_quadprog(copy_hessian, copy_gradient,
+								 eq_constraint_mat, eq_bound,
+								 ineq_constraint_mat, ineq_bound,
+								 solution_);
+
 	return true;
 }
 
