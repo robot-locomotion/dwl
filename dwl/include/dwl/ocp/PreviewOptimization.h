4b2dbab6fdb51f25a4816ef0db409b01aecb0536
Carlos Mastalli <carlos.mastalli@iit.it>
2016-01-05 18:32:20 +0100

    Added an initial version of the PreviewOptimization class

---
+++ dwl/include/dwl/ocp/PreviewOptimization.h
@@ -0,0 +1,86 @@
+#ifndef DWL__OCP__PREVIEW_OPTIMIZATION__H
+#define DWL__OCP__PREVIEW_OPTIMIZATION__H
+
+#include <dwl/model/OptimizationModel.h>
+#include <dwl/simulation/PreviewLocomotion.h>
+
+namespace dwl
+{
+
+namespace ocp
+{
+
+/**
+ * @class PreviewOptimization
+ * @brief A preview optimization problem requires information of "modeling" constraint and "goals"
+ * cost functions. A modeling constraints establishes the desired locomotion preview such as
+ * stability constraints (i.e. CoP/ZMP inside the support region). These constraints could
+ * implemented as soft or hard constraints. On the other hand, a goal cost evaluates the performance
+ * of a specific behavior preview such as: stance duration, step distance or smoothing of CoM.
+ */
+class PreviewOptimization : public model::OptimizationModel
+{
+	public:
+		/** @brief Constructor function */
+		PreviewOptimization();
+
+		/** @brief Destructor function */
+		~PreviewOptimization();
+
+		/** @brief Initializes the optimization model, i.e. the dimensions of the optimization
+		 * vectors */
+		void init();
+
+		/**
+		 * @brief Sets the initial sequence of preview control parameters
+		 * @param const simulation::MultiPhasePreviewControl& Initial sequence of preview
+		 * control parameters
+		 */
+		void setStartingPreviewControl(const simulation::MultiPhasePreviewControl& control);
+
+		/**
+		 * @brief Gets the starting point of the problem
+		 * @param Eigen::Ref<Eigen::VectorXd> Full initial point
+		 */
+		void getStartingPoint(Eigen::Ref<Eigen::VectorXd> full_initial_point);
+
+		/**
+		 * @brief Evaluates the bounds of the problem
+		 * @param Eigen::Ref<Eigen::VectorXd> Full state lower bound
+		 * @param Eigen::Ref<Eigen::VectorXd> Full state upper bound
+		 * @param Eigen::Ref<Eigen::VectorXd> Full constraint lower bound
+		 * @param Eigen::Ref<Eigen::VectorXd> Full constraint upper bound
+		 */
+		void evaluateBounds(Eigen::Ref<Eigen::VectorXd> full_state_lower_bound,
+							Eigen::Ref<Eigen::VectorXd> full_state_upper_bound,
+							Eigen::Ref<Eigen::VectorXd> full_constraint_lower_bound,
+							Eigen::Ref<Eigen::VectorXd> full_constraint_upper_bound);
+		/**
+		 * @brief Evaluates the constraint function given a current decision state
+		 * @param Eigen::Ref<Eigen::VectorXd> Constraint vector
+		 * @param const Eigen::Ref<const Eigen:VectorXd>& Decision vector
+		 */
+		void evaluateConstraints(Eigen::Ref<Eigen::VectorXd> full_constraint,
+								 const Eigen::Ref<const Eigen::VectorXd>& decision_var);
+
+		/**
+		 * @brief Evaluates the cost function given a current decision state
+		 * @param double& Cost value
+		 * @param const Eigen::Ref<const Eigen:VectorXd>& Decision vector
+		 */
+		void evaluateCosts(double& cost,
+						   const Eigen::Ref<const Eigen::VectorXd>& decision_var);
+
+		/**
+		 * @brief Evaluates the solution from an optimizer
+		 * @param const Eigen::Ref<const Eigen::VectorXd>& Solution vector
+		 * @return WholeBodyTrajectory& Returns the whole-body trajectory solution
+		 */
+		WholeBodyTrajectory& evaluateSolution(const Eigen::Ref<const Eigen::VectorXd>& solution);
+};
+
+} //@namespace ocp
+} //@namespace dwl
+
+
+#endif
