ab05d1d168f31cf07325c963cf05461e98c4f77b
Carlos Mastalli <carlos.mastalli@iit.it>
2016-01-17 19:53:35 +0100

    Computed the step distance cost. Defined methods for setting the cost weights

--- dwl/include/dwl/ocp/PreviewOptimization.h
+++ dwl/include/dwl/ocp/PreviewOptimization.h
@@ -45,6 +45,24 @@ class PreviewOptimization : public model::OptimizationModel
 		void setStartingPreviewControl(const simulation::PreviewControl& control);
 
 		/**
+		 * @brief Sets the step cost weights
+		 * @param double Step duration weight
+		 * @param double Step distance weight
+		 */
+		void setStepWeights(double time_weight,
+							double distance_weight);
+
+		/**
+		 * @brief Sets the CoM acceleration weights
+		 * @param double Weight in the x-axis acceleration
+		 * @param double Weight in the y-axis acceleration
+		 * @param double Weight in the z-axis acceleration
+		 */
+		void setComAccelerationWeight(double x_weight,
+									  double y_weight,
+									  double z_weight);
+
+		/**
 		 * @brief Gets the starting point of the problem
 		 * @param Eigen::Ref<Eigen::VectorXd> Full initial point
 		 */
@@ -95,6 +113,10 @@ class PreviewOptimization : public model::OptimizationModel
 								   const simulation::PreviewControl& preview_control);
 		simulation::PreviewLocomotion preview_;
 		simulation::PreviewState actual_state_;
+
+		double step_time_weight_;
+		double step_dist_weight_;
+		Eigen::Vector3d acc_int_weight_;
 };
 
 } //@namespace ocp
