80162bcac3c835d8941a67b504acc0da6d89c8c5
Carlos Mastalli <carlos.mastalli@iit.it>
2016-07-26 12:45:56 +0200

    [core] Defined a class for frame transformation, i.e. world, base and horizontal frames

---
+++ dwl/include/dwl/utils/FrameTF.h
@@ -0,0 +1,90 @@
+#ifndef DWL__MATH__FRAME_TF__H
+#define DWL__MATH__FRAME_TF__H
+
+#include <dwl/utils/Orientation.h>
+
+
+namespace dwl
+{
+
+namespace math
+{
+
+class FrameTF
+{
+	public:
+		/** @brief Constructor function */
+		FrameTF();
+
+		/** @brief Destructor function */
+		~FrameTF();
+
+		/** @brief Transforms the defined vector in the world into the base
+		 * frame given the orientation of the body: RPY angles or quaternion */
+		Eigen::Vector3d fromWorldToBaseFrame(const Eigen::Vector3d& vec_W,
+											 const Eigen::Vector3d& rpy) const;
+		Eigen::Vector3d fromWorldToBaseFrame(const Eigen::Vector3d& vec_W,
+											 const Eigen::Quaterniond& q) const;
+
+		/** @brief Transforms the defined vector in the world into the horizontal
+		 * frame given the orientation of the body: RPY angles or quaternion */
+		Eigen::Vector3d fromWorldToHorizontalFrame(const Eigen::Vector3d& vec_W,
+												   const Eigen::Vector3d& rpy) const;
+		Eigen::Vector3d fromWorldToHorizontalFrame(const Eigen::Vector3d& vec_W,
+												   const Eigen::Quaterniond& q) const;
+
+		/** @brief Transforms the defined vector in the base into the world
+		 * frame given the orientation of the body: RPY angles or quaternion */
+		Eigen::Vector3d fromBaseToWorldFrame(const Eigen::Vector3d& vec_B,
+											 const Eigen::Vector3d& rpy) const;
+		Eigen::Vector3d fromBaseToWorldFrame(const Eigen::Vector3d& vec_B,
+											 const Eigen::Quaterniond& q) const;
+
+		/** @brief Transforms the defined vector in the base into the horizontal
+		 * frame given the orientation of the body: RPY angles or quaternion */
+		Eigen::Vector3d fromBaseToHorizontalFrame(const Eigen::Vector3d& vec_B,
+												  const Eigen::Vector3d& rpy) const;
+		Eigen::Vector3d fromBaseToHorizontalFrame(const Eigen::Vector3d& vec_B,
+												  const Eigen::Quaterniond& q) const;
+
+		/** @brief Transforms the defined vector in the horizontal into the world
+		 * frame given the orientation of the body: RPY angles or quaternion */
+		Eigen::Vector3d fromHorizontalToWorldFrame(const Eigen::Vector3d& vec_H,
+												   const Eigen::Vector3d& rpy) const;
+		Eigen::Vector3d fromHorizontalToWorldFrame(const Eigen::Vector3d& vec_H,
+												   const Eigen::Quaterniond& q) const;
+
+		/** @brief Transforms the defined vector in the horizontal into the base
+		 * frame given the orientation of the body: RPY angles or quaternion */
+		Eigen::Vector3d fromHorizontalToBaseFrame(const Eigen::Vector3d& vec_H,
+												  const Eigen::Vector3d& rpy) const;
+		Eigen::Vector3d fromHorizontalToBaseFrame(const Eigen::Vector3d& vec_H,
+												  const Eigen::Quaterniond& q) const;
+
+
+	private:
+		/** @brief Computes the rotation matrix from world to horizontal frame */
+		Eigen::Matrix3d inline getRotWorldToHF(const Eigen::Vector3d& rpy) const {
+			// Note that the roll and pitch components are zero w.r.t. the
+			// world frame
+			Eigen::Vector3d rpy_H(0., 0., rpy(2));
+			return math::getRotationMatrix(rpy_H);
+		}
+
+		/** @brief Computes the rotation matrix from world to horizontal frame */
+		Eigen::Matrix3d inline getRotBaseToHF(const Eigen::Vector3d& rpy) const {
+			// Note that the yaw component is zero w.r.t. the base frame
+			Eigen::Matrix3d R;
+
+			R <<  cos(rpy(1)),  sin(rpy(0))*sin(rpy(1)),  cos(rpy(0))*sin(rpy(1)),
+					       0.,              cos(rpy(0)),             -sin(rpy(0)),
+				 -sin(rpy(1)),  sin(rpy(0))*cos(rpy(1)),  cos(rpy(0))*cos(rpy(1));
+
+			return R;
+		}
+};
+
+} //@namespace math
+} //@namespace dwl
+
+#endif
---
+++ dwl/src/utils/FrameTF.cpp
@@ -0,0 +1,94 @@
+#include <dwl/utils/FrameTF.h>
+#include <iostream>
+
+namespace dwl
+{
+
+namespace math
+{
+
+FrameTF::FrameTF()
+{
+
+}
+
+
+FrameTF::~FrameTF()
+{
+
+}
+
+
+Eigen::Vector3d FrameTF::fromWorldToBaseFrame(const Eigen::Vector3d& vec_W,
+											  const Eigen::Vector3d& rpy) const
+{
+	return math::getQuaternion(rpy).inverse().toRotationMatrix() * vec_W;
+}
+
+
+Eigen::Vector3d FrameTF::fromWorldToBaseFrame(const Eigen::Vector3d& vec_W,
+											  const Eigen::Quaterniond& q) const
+{
+	return q.inverse().toRotationMatrix() * vec_W;
+}
+
+
+Eigen::Vector3d FrameTF::fromWorldToHorizontalFrame(const Eigen::Vector3d& vec_W,
+													const Eigen::Vector3d& rpy) const
+{
+	return getRotWorldToHF(rpy) * vec_W;
+}
+
+
+Eigen::Vector3d FrameTF::fromWorldToHorizontalFrame(const Eigen::Vector3d& vec_W,
+													const Eigen::Quaterniond& q) const
+{
+	return getRotWorldToHF(math::getRPY(q)) * vec_W;
+}
+
+
+Eigen::Vector3d FrameTF::fromBaseToWorldFrame(const Eigen::Vector3d& vec_B,
+											  const Eigen::Vector3d& rpy) const
+{
+	return math::getQuaternion(rpy).toRotationMatrix() * vec_B;
+}
+
+
+Eigen::Vector3d FrameTF::fromBaseToWorldFrame(const Eigen::Vector3d& vec_B,
+											  const Eigen::Quaterniond& q) const
+{
+	return q.toRotationMatrix() * vec_B;
+}
+
+
+Eigen::Vector3d FrameTF::fromBaseToHorizontalFrame(const Eigen::Vector3d& vec_B,
+												   const Eigen::Vector3d& rpy) const
+{
+	return getRotBaseToHF(rpy) * vec_B;
+}
+
+
+Eigen::Vector3d FrameTF::fromBaseToHorizontalFrame(const Eigen::Vector3d& vec_B,
+												   const Eigen::Quaterniond& q) const
+{
+	Eigen::Vector3d rpy = math::getRPY(q);
+	return getRotBaseToHF(rpy) * vec_B;
+}
+
+
+Eigen::Vector3d FrameTF::fromHorizontalToWorldFrame(const Eigen::Vector3d& vec_H,
+													const Eigen::Vector3d& rpy) const
+{
+	return getRotWorldToHF(rpy).inverse() * vec_H;
+}
+
+
+Eigen::Vector3d FrameTF::fromHorizontalToWorldFrame(const Eigen::Vector3d& vec_H,
+													const Eigen::Quaterniond& q) const
+{
+	Eigen::Vector3d rpy = math::getRPY(q);
+	return getRotWorldToHF(rpy).inverse() * vec_H;
+}
+
+} //@namespace math
+} //@namespace dwl
