e7af8d4f07e599cf163ee36d2a809e840f738b94
Carlos Mastalli <carlos.mastalli@iit.it>
2017-08-31 17:14:03 +0200

    [bugfix] Fixed an issue finding the qpOASES library

--- dwl/cmake/FindQPOASES.cmake
+++ dwl/cmake/FindQPOASES.cmake
@@ -12,12 +12,14 @@
 # Copyright (c) 2014-2018 Carlos Mastalli, <carlos.mastalli@iit.it>
 # Redistribution and use is allowed according to the terms of the XXX license.
 
-set(QPOASES_DIR ${PROJECT_SOURCE_DIR}/thirdparty/qpOASES)
+set(QPOASES_DIR  /usr/local/)
+#set(QPOASES_DIR  ${PROJECT_SOURCE_DIR}/thirdparty/qpOASES)
 
 set(QPOASES_INCLUDE_DIRS ${QPOASES_DIR}/include)
-set(QPOASES_LIBRARY_DIR ${QPOASES_DIR}/bin)
-find_library(QPOASES_LIBRARIES qpOASES ${QPOASES_LIBRARY_DIR}
-									   NO_DEFAULT_PATH)
+set(QPOASES_LIBRARY_DIR ${QPOASES_DIR}/lib)
+find_library(QPOASES_LIBRARIES NAMES  qpOASES
+							   PATHS  ${QPOASES_LIBRARY_DIR}
+									  NO_DEFAULT_PATH)
 
 include(FindPackageHandleStandardArgs)
 find_package_handle_standard_args(QPOASES DEFAULT_MSG QPOASES_LIBRARIES QPOASES_INCLUDE_DIRS)
