451182a2e6d82cb7bd0dd51649387c7f6c5959b2
Carlos Mastalli <carlos.mastalli@laas.fr>
2017-12-14 18:26:39 +0100

    [cleanup] Checking if you install the 3rd libraries in the common root

--- dwl/install_deps.sh
+++ dwl/install_deps.sh
@@ -622,7 +622,7 @@ sudo apt-get install doxygen
 ##---------------------------------------------------------------##
 echo ""
 echo -e "${COLOR_BOLD}Installing Eigen ...${COLOR_RESET}"
-if [ -d "$INSTALL_DEPS_PREFIX/include/eigen3" ]; then
+if [ -d "$INSTALL_DEPS_PREFIX/include/eigen3" ] || [ -d "$COMMON_INSTALL_PREFIX/include/eigen3" ]; then
 	echo -e -n "${COLOR_QUES}Do you want to re-install Eigen 3.2.10? [y/N]: ${COLOR_RESET}"
 	read ANSWER_EIGEN
 	if [ "$ANSWER_EIGEN" == "Y" ] || [ "$ANSWER_EIGEN" == "y" ]; then
@@ -675,7 +675,7 @@ fi
 ##---------------------------------------------------------------##
 echo ""
 echo -e "${COLOR_BOLD}Installing RBDL ...${COLOR_RESET}"
-if [ -d "$INSTALL_DEPS_PREFIX/include/rbdl" ]; then
+if [ -d "$INSTALL_DEPS_PREFIX/include/rbdl" ] || [ -d "$COMMON_INSTALL_PREFIX/include/rbdl" ]; then
 	echo -e -n "${COLOR_QUES}Do you want to re-install RBDL 2.4.0? [y/N]: ${COLOR_RESET}"
 	read ANSWER_RBDL
 	if [ "$ANSWER_RBDL" == "Y" ] || [ "$ANSWER_RBDL" == "y" ]; then
@@ -708,7 +708,7 @@ fi
 ##---------------------------------------------------------------##
 echo ""
 echo -e "${COLOR_BOLD}Installing SWIG ...${COLOR_RESET}"
-if [ -d "/usr/share/swig" ]; then
+if [ -d "$INSTALL_DEPS_PREFIX/share/swig" ] || [ -d "$COMMON_INSTALL_PREFIX/share/swig" ]; then
 	echo -e -n "${COLOR_QUES}Do you want to re-install SWIG 3.0.12? [y/N]: ${COLOR_RESET}"
 	read ANSWER_SWIG
 	if [ "$ANSWER_SWIG" == "Y" ] || [ "$ANSWER_SWIG" == "y" ]; then
@@ -724,7 +724,7 @@ fi
 ##---------------------------------------------------------------##
 echo ""
 echo -e "${COLOR_BOLD}Installing Ipopt ...${COLOR_RESET}"
-if [ -d "$INSTALL_DEPS_PREFIX/include/coin" ]; then
+if [ -d "$INSTALL_DEPS_PREFIX/include/coin" ] || [ -d "$COMMON_INSTALL_PREFIX/include/coin" ]; then
 	# Control will enter here if $DIRECTORY exists.
 	echo -e -n "${COLOR_QUES}Do you want to re-install Ipopt 3.12.4? [y/N]: ${COLOR_RESET}"
 	read ANSWER_IPOPT
@@ -745,7 +745,7 @@ fi
 ##---------------------------------------------------------------##
 echo ""
 echo -e "${COLOR_BOLD}Installing qpOASES ...${COLOR_RESET}"
-if [ -d "qpOASES" ]; then
+if [ -d "$INSTALL_DEPS_PREFIX/include/qpOASES" ] || [ -d "$COMMON_INSTALL_PREFIX/include/qpOASES" ]; then
 	# Control will enter here if $DIRECTORY exists.
 	echo -e -n "${COLOR_QUES}Do you want to re-install qpOASES 3.2.0? [y/N]: ${COLOR_RESET}"
 	read ANSWER_QPOASES
@@ -766,7 +766,7 @@ fi
 ##---------------------------------------------------------------##
 echo ""
 echo -e "${COLOR_BOLD}Installing libcmaes ...${COLOR_RESET}"
-if [ -d "$INSTALL_DEPS_PREFIX/include/libcmaes" ]; then
+if [ -d "$INSTALL_DEPS_PREFIX/include/libcmaes" ] || [ -d "$COMMON_INSTALL_PREFIX/include/yaml-cpp" ]; then
 	# Control will enter here if $DIRECTORY exists.
 	echo -e -n "${COLOR_QUES}Do you want to re-install libcmaes 0.9.5? [y/N]: ${COLOR_RESET}"
 	read ANSWER_LIBCMAES
