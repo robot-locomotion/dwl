47f6690a0ce931a409f48fec6225a4165f49f6c7
Carlos Mastalli <carlos.mastalli@laas.fr>
2017-12-15 14:26:49 +0100

    [cleanup] Removed the preview optimization for the test executable

--- dwl/tests/cmaesFamilyDWLTest.cpp
+++ dwl/tests/cmaesFamilyDWLTest.cpp
@@ -1,5 +1,4 @@
 #include <dwl/ocp/OptimalControl.h>
-#include <dwl/ocp/PreviewOptimization.h>
 #include <dwl/solver/OptimizationSolver.h>
 #include <dwl/solver/cmaesSOFamily.h>
 #include <model/HS071DynamicalSystem.cpp>
@@ -9,24 +8,18 @@
 
 int main(int argc, char **argv)
 {
-	bool op = true;
 	dwl::solver::OptimizationSolver* solver = new dwl::solver::cmaesSOFamily<>();
 	dwl::ocp::OptimalControl optimal_control;
-	dwl::ocp::PreviewOptimization preview_opt;
-	if (op) {
-		solver->setOptimizationModel(&optimal_control);
+	solver->setOptimizationModel(&optimal_control);
 
-		dwl::ocp::DynamicalSystem* dynamical_system = new dwl::model::HS071DynamicalSystem();
-		dynamical_system->defineAsSoftConstraint();
-		dwl::ocp::SoftConstraintProperties properties(10000, 0.1, dwl::ocp::QUADRATIC);
-		dynamical_system->setSoftProperties(properties);
-		dwl::ocp::Cost* cost = new dwl::model::HS071Cost();
+	dwl::ocp::DynamicalSystem* dynamical_system = new dwl::model::HS071DynamicalSystem();
+	dynamical_system->defineAsSoftConstraint();
+	dwl::ocp::SoftConstraintProperties properties(10000, 0.1, dwl::ocp::QUADRATIC);
+	dynamical_system->setSoftProperties(properties);
+	dwl::ocp::Cost* cost = new dwl::model::HS071Cost();
 
-		optimal_control.addDynamicalSystem(dynamical_system);
-		optimal_control.addCost(cost);
-	} else {
-		solver->setOptimizationModel(&preview_opt);
-	}
+	optimal_control.addDynamicalSystem(dynamical_system);
+	optimal_control.addCost(cost);
 
 	solver->setFromConfigFile("../config/cmaes_config.yaml");
 	solver->init();
