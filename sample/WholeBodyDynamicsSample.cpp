9f09487231fc9b44f783369c0f94a4f192472748
Carlos Mastalli <carlos.mastalli@iit.it>
2018-02-12 14:23:58 +0100

    [sample] Added the initialization of the kinematics for learning purposes

--- sample/WholeBodyDynamicsSample.cpp
+++ sample/WholeBodyDynamicsSample.cpp
@@ -14,6 +14,7 @@ int main(int argc, char **argv)
 	std::string urdf_file = DWL_SOURCE_DIR"/sample/hyq.urdf";
 	std::string yarf_file = DWL_SOURCE_DIR"/config/hyq.yarf";
 	fbs.resetFromURDFFile(urdf_file, yarf_file);
+	wkin.reset(fbs);
 	wdyn.reset(fbs, wkin);
 
 	// Define the DoF after initializing the robot model
