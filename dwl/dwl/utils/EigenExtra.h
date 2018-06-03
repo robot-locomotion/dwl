cc6f814aaf167fb87f31e2729f987905b0e7bb4c
Carlos Mastalli <carlos.mastalli@laas.fr>
2018-06-03 12:02:42 +0200

    [utils] Added other Matrix typedef

--- dwl/dwl/utils/EigenExtra.h
+++ dwl/dwl/utils/EigenExtra.h
@@ -10,7 +10,9 @@ namespace Eigen
 
 typedef Eigen::Matrix<double,6,1> Vector6d;
 typedef Eigen::Matrix<double,7,1> Vector7d;
+typedef Eigen::Matrix<double,4,4> Matrix4d;
 typedef Eigen::Matrix<double,6,6> Matrix6d;
+typedef Eigen::Matrix<double,6,Eigen::Dynamic> Matrix6x;
 typedef std::map<std::string,Eigen::Vector3d> Vector3dMap;
 typedef std::map<std::string,Eigen::Vector6d> Vector6dMap;
 typedef std::map<std::string,Eigen::Vector7d> Vector7dMap;
