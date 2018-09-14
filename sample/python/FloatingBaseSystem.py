9def2587f99edec1f1840f4ee963b7b5ceca64eb
Carlos Mastalli <carlos.mastalli@laas.fr>
2018-09-14 19:13:57 +0200

    [python] Updated the floating base python script

--- sample/python/FloatingBaseSystem.py
+++ sample/python/FloatingBaseSystem.py
@@ -13,7 +13,7 @@ fbs = dwl.FloatingBaseSystem()
 ws = dwl.WholeBodyState()
 
 # Initializing the URDF model and whole-body state
-fpath = os.path.dirname(os.path.abspath(__file__))
+fpath = str(os.path.dirname(os.path.abspath(__file__)))
 fbs.resetFromURDFFile(fpath + '/../../models/hyq.urdf', fpath + '/../../models/hyq.yarf')
 ws.setJointDoF(fbs.getJointDoF())
 
@@ -38,47 +38,77 @@ ws.setJointPosition(1.5, fbs.getJointId('rh_kfe_joint'))
 # Getting the total mass of the system. Note that you could also get the mass of a specific
 # body (e.g. sys.getBodyMass(body_name))
 print()
-print('Total mass:', fbs.getTotalMass())
+print('total mass:', fbs.getTotalMass())
 print('lf_upperleg mass:', fbs.getBodyMass('lf_upperleg'))
-print('The gravity acceleration is', fbs.getGravityAcceleration())
+print('gravity acceleration is', fbs.getGravityAcceleration())
+
 
 
 
 # Getting the CoM of the floating-base body. Note that you could also get the CoM of a
 # specific body (e.g. sys.getBodyCoM(body_name))
 print()
-print('Floating-base CoM:', fbs.getFloatingBaseCoM().transpose())
+print('floating-base CoM:', fbs.getFloatingBaseCoM().transpose())
 print('lf_upperleg CoM:', fbs.getBodyCoM('lf_upperleg').transpose())
+
+
 
 
 # Getting the number of system DoF, floating-base Dof, joints and end-effectors
 print()
 print('NQ:', fbs.getConfigurationDim())
 print('NV:', fbs.getTangentDim())
-print('Number of joint:', fbs.getJointDoF())
-print('Number of end-effectors:', fbs.getNumberOfEndEffectors())
-print('The floating-base body name:', fbs.getFloatingBaseName())
+print('number of joint:', fbs.getJointDoF())
+print('number of end-effectors:', fbs.getNumberOfEndEffectors())
+print('floating-base body name:', fbs.getFloatingBaseName())
+
+
 
 
 # Getting the joint names and ids
 print()
 joints = fbs.getJoints()
 for name in joints:
-	print('Joint[', joints[name], '] =', name)
-
-
+	print('joint[', joints[name], '] =', name)
 print()
-for name in fbs.getJointNames():
-	print('The joint names are', name)
-print('The joint name of 3 is', fbs.getJointName(3))
+print('joint names are', fbs.getJointList())
+print('joint name of 3 is', fbs.getJointName(3))
+
+
+
+
+# Getting the body names and ids
+print()
+bodies = fbs.getBodies()
+for name in bodies:
+    print('body[', bodies[name], '] =', name)
+print()
+print('body names are', fbs.getBodyList())
+print('body name of 3 is', fbs.getBodyName(3))
+
+
+
+
+# Getting the frame names and ids
+print()
+frames = fbs.getFrames()
+for name in frames:
+    print('frame[', frames[name], '] =', name)
+print()
+print('frame names are', fbs.getFrameList())
+print('frame name of 3 is', fbs.getFrameName(3))
+
+
 
 
 # Getting the end-effector names and ids
 print()
 contacts = fbs.getEndEffectors()
-print('The number of feet are', fbs.getNumberOfEndEffectors(dwl.FOOT))
+print('number of feet are', fbs.getNumberOfEndEffectors(dwl.FOOT))
 for name in contacts:
-	print('End-effector[', contacts[name], '] =', name)
+	print('end-effector[', contacts[name], '] =', name)
+
+
 
 
 # Getting the joint limits
@@ -89,10 +119,14 @@ print('velocity limits =', fbs.getVelocityLimits().transpose())
 print('effort limits =', fbs.getEffortLimits().transpose())
 
 
+
+
 # Getting the default posture
 print()
 joint_pos0 = fbs.getDefaultPosture()
-print('The nominal joint positions =', joint_pos0.transpose())
+print('nominal joint positions =', joint_pos0.transpose())
+
+
 
 
 # Converting between whole-body state to generalized state, and viceverse
@@ -101,17 +135,19 @@ base_state = np.zeros(6)
 joint_state = joint_pos0
 generalized_state = fbs.toConfigurationState(ws.getBaseSE3(), ws.getJointPosition())
 print('Converting the whole-body pose to a configuration vector')
-print('Base state =', ws.getBaseSE3())
-print('Joint state =', ws.getJointPosition().transpose())
-print('The configuration vector =', generalized_state.transpose())
+print('base state =', ws.getBaseSE3())
+print('joint state =', ws.getJointPosition().transpose())
+print('configuration vector =', generalized_state.transpose())
+
+
 
 
 # Setting up the branch states
 print()
 joint_pos = ws.getJointPosition()
 lf_branch_pos = np.array([0.5, 0.75, 1.5]);
-fbs.setBranchState(joint_pos, lf_branch_pos, 'lf_foot');
+fbs.setBranchState(joint_pos, lf_branch_pos, 'lf_foot')
 ws.setJointPosition(joint_pos)
 print('Setting up the lf_foot branch position =', lf_branch_pos.transpose())
-print('Joint_position =', ws.getJointPosition().transpose())
-print('The lf_foot branch position =', fbs.getBranchState(ws.getJointPosition(), 'lf_foot').transpose())
+print('joint_position =', ws.getJointPosition().transpose())
+print('lf_foot branch position =', fbs.getBranchState(ws.getJointPosition(), 'lf_foot').transpose())
