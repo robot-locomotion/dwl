e4eb66bcc898d140363ac8df80f1442e7461f4dc
Carlos Mastalli <carlos.mastalli@laas.fr>
2018-09-27 11:05:17 +0200

    [core] Included mapping for Eigen::Vector2d

--- dwl/dwl/utils/EigenExtra.h
+++ dwl/dwl/utils/EigenExtra.h
@@ -13,6 +13,7 @@ typedef Eigen::Matrix<double,7,1> Vector7d;
 typedef Eigen::Matrix<double,4,4> Matrix4d;
 typedef Eigen::Matrix<double,6,6> Matrix6d;
 typedef Eigen::Matrix<double,6,Eigen::Dynamic> Matrix6x;
+typedef std::map<std::string,Eigen::Vector2d> Vector2dMap;
 typedef std::map<std::string,Eigen::Vector3d> Vector3dMap;
 typedef std::map<std::string,Eigen::Vector6d> Vector6dMap;
 typedef std::map<std::string,Eigen::Vector7d> Vector7dMap;
